7acabf52bf5b3f29b551225d40d517dd8b038895
robertoarteaga <robert-art1@hotmail.com>
2018-09-14 14:59:07 -0500

    Añadí inicio y registro de sesión

---
+++ Hola Mundo en Git/registroGastos/public/js/funcionalidad.js
@@ -0,0 +1,9 @@
+
+function mostrarRegistro() {
+	document.getElementById('inicio').style.display = "none";
+	document.getElementById('registro').style.display = "flex";
+}
+function mostrarInicio() {
+	document.getElementById('inicio').style.display = "flex";
+	document.getElementById('registro').style.display = "none";
+}
